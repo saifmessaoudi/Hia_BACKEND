e3a04aedffd1ed2837973d33a717bbe26dd18693
saifmessaoudi <saif.messaoudi@esprit.tn>
2024-08-20 20:15:17 +0100

    fox

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ import reservationRouter from './routes/reservation.route.js';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT ||3030;
  
 
 
