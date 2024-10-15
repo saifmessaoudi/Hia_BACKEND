d7eae0cf3899e11a4dc807a8ea38f4b0007c7de3
saifmessaoudi <saif.messaoudi@esprit.tn>
2024-10-15 16:07:43 +0100

    fix

--- config/database.config.js
+++ config/database.config.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI_LOCAL);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connection SUCCESS");
     } catch (error) {
         console.error("MongoDB connection FAIL : " + error);
